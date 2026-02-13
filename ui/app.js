9bafd13b7fe280dbdadf5bf4ceb3eb27e18caa2e
SaiJeevanPuchakayala <saijeevan2002@gmail.com>
2026-02-13 12:15:20 +0530

    Improved UI/UX

--- ui/app.js
+++ ui/app.js
@@ -190,7 +190,7 @@ class TeachingAssistantApp {
         }
 
         container.innerHTML = files.map(f =>
-            `<div class="file-item"><span class="file-icon">&#9679;</span> ${f}</div>`
+            `<div class="file-item"><svg class="file-icon" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"/><polyline points="13 2 13 9 20 9"/></svg>${f}</div>`
         ).join('');
     }
 
